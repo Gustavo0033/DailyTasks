18fc3462e46018dfe83a0231993cc14dabaa8ea5
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2024-10-21 17:52:20 -0300

    App completed

---
+++ CRUD-Realm/DataModel/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  CRUD-Realm
+//
+//  Created by Gustavo Mendonca on 18/10/24.
+//
+
+import Foundation
---
+++ CRUD-Realm/DataModel/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  CRUD-Realm
+//
+//  Created by Gustavo Mendonca on 18/10/24.
+//
+
+import Foundation
